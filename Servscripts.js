5d7a5ff811d913637bba62bcac7185a47b989399
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-02-05 16:57:58 +0500

    Add files via upload

---
+++ Servscripts.js
@@ -0,0 +1,3 @@
+const service = ["Post-operative Rehabilitation", "Chronic Illness Management", "Sports injury Recovery"];
+document.getElementById("ser").innerHTML = service;
+//i created this js file because it doesn't work in the main file
